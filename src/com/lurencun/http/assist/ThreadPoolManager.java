2042fa573666bbaa67c79978d265900abf4d5dfc
chenyoca <chenyoca@gmail.com>
2013-01-16 01:53:28 +0800

    Fixed a bug

--- src/com/lurencun/http/assist/ThreadPoolManager.java
+++ src/com/lurencun/http/assist/ThreadPoolManager.java
@@ -25,8 +25,8 @@ public class ThreadPoolManager {
 	public final static int KEEP_ALIVE_TIME_IN_S = 3 * 10;
 	private final static int TASK_SCHEDULE_DELAY = 6 * 1000;
 	
-	private ConcurrentHashMap<Integer, Future<?>> mTaskHolder;
-	private int mRequestTaskIdPool;
+	private ConcurrentHashMap<Integer, Future<?>> taskHolder;
+	private int requestTaskIdPool;
 	private final ThreadPoolExecutor executor ;
 	private final Timer cleanNullReferenceDaemon;
 	
@@ -36,18 +36,18 @@ public class ThreadPoolManager {
 				CORE_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME_IN_S, TimeUnit.SECONDS,
 				queue, new ThreadPoolExecutor.CallerRunsPolicy());
 		cleanNullReferenceDaemon = new Timer(true);
-		mTaskHolder = new ConcurrentHashMap<Integer, Future<?>>();
+		taskHolder = new ConcurrentHashMap<Integer, Future<?>>();
 		// 开启空引用自检任务
 		cleanNullReferenceDaemon.schedule(new TimerTask() {
 			@Override
 			public void run() {
-				Iterator<Map.Entry<Integer, Future<?>>> iter = mTaskHolder.entrySet().iterator();
+				Iterator<Map.Entry<Integer, Future<?>>> iter = taskHolder.entrySet().iterator();
 				while (iter.hasNext()) {
 					Map.Entry<Integer, Future<?>> entry = (Map.Entry<Integer, Future<?>>) iter.next();
 					Future<?> task = entry.getValue();
 					int key = entry.getKey();
 					if(task.isDone() || task.isCancelled()){
-						mTaskHolder.remove(key);
+						taskHolder.remove(key);
 					}
 				}
 			}
@@ -60,9 +60,9 @@ public class ThreadPoolManager {
 	 * @return 一个任务包装对象
 	 */
 	public int submit(Runnable r){
-		int taskId = mRequestTaskIdPool++;
+		int taskId = requestTaskIdPool++;
 		Future<?> task = executor.submit(r);
-		mTaskHolder.put(taskId, task);
+		taskHolder.put(taskId, task);
 		return taskId;
 	}
 	
@@ -72,8 +72,8 @@ public class ThreadPoolManager {
 	 * @param interruptRunning 是否中断任务
 	 */
 	public void cancel(int taskId,boolean interruptRunning){
-		if(mTaskHolder.containsKey(taskId)){
-			Future<?> task = mTaskHolder.get(taskId);
+		if(taskHolder.containsKey(taskId)){
+			Future<?> task = taskHolder.get(taskId);
 			if(!task.isDone() && !task.isCancelled()){
 				task.cancel(interruptRunning);
 			}
